676ae50d5baddfd0d72686cafc61413089b6f16e
Arjun KayalMoni <arjun.umathanu@gmail.com>
2023-04-25 14:01:21 +0530

    installed prettier and eslint integration plugins and configs

--- src/components/greet.tsx
+++ src/components/greet.tsx
@@ -1,9 +1,22 @@
-export default function Greet() {
-    const items: string[] = ["Vite", "TypeScript", "Path Alias", "Vitest", "Eslint", "Prettier", "React-Testing Library", "Cypress"]
+export default function Greet(): JSX.Element {
+    const items: string[] = [
+        'Vite',
+        'TypeScript',
+        'Path Alias',
+        'Vitest',
+        'Eslint',
+        'Prettier',
+        'React-Testing Library',
+        'Cypress',
+    ];
     return (
         <>
             <h3>Setting up</h3>
-            <ul>{items.map(item => <li>{item}</li>)}</ul>
+            <ul>
+                {items.map((item) => (
+                    <li key={item}>{item}</li>
+                ))}
+            </ul>
         </>
-    )
+    );
 }
